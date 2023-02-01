ef127df07f45044a4bb06fdbda54cd9487805221
chainsmoker-codes <himeshkumardns@gmail.com>
2023-02-01 18:00:03 +0530

    Update nifty.js

--- src/nifty.js
+++ src/nifty.js
@@ -6,13 +6,16 @@ const nse = new NseIndia()
 async function nifty() {
     const data = await nse.getDataByEndpoint("/api/allIndices")
     const reqdata = data.data[0]
+    var nick
 
     if(reqdata.percentChange.toString() == `+`) {
         roles = ['1070278187407388763', '1011635113928429651', '1070274725189787682', '1070308288404672612']
         nick = `▲`
+        console.log(`this`)
     } else if (reqdata.percentChange.toString() == `-`) {
         nick = `▼`
         roles = ['1070278187407388763', '1011635113928429651', '1070274725189787682', '1070308485058801724']
+        console.log(`that`)
     }
 
     await fetch(`https://discord.com/api/guilds/${process.env.guild_id}/members/${process.env.nifty_id}`, {
