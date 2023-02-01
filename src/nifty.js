f42a0b5e0d4e265f3dc54b9378c3f2d9dd0fc17c
chainsmoker-codes <himeshkumardns@gmail.com>
2023-02-01 17:58:44 +0530

    Update nifty.js

--- src/nifty.js
+++ src/nifty.js
@@ -7,10 +7,10 @@ async function nifty() {
     const data = await nse.getDataByEndpoint("/api/allIndices")
     const reqdata = data.data[0]
 
-    if(reqdata.percentChange[0].toString() == `+`) {
+    if(reqdata.percentChange.toString() == `+`) {
         roles = ['1070278187407388763', '1011635113928429651', '1070274725189787682', '1070308288404672612']
         nick = `▲`
-    } else if (reqdata.percentChange[0].toString() == `-`) {
+    } else if (reqdata.percentChange.toString() == `-`) {
         nick = `▼`
         roles = ['1070278187407388763', '1011635113928429651', '1070274725189787682', '1070308485058801724']
     }
