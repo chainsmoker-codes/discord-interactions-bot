4fa817e6d40d7a9926a410cffb425a5a8e0b724e
chainsmoker-codes <himeshkumardns@gmail.com>
2023-02-02 18:54:06 +0530

    Update bitcoin.js

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -17,14 +17,11 @@ async function bitchcoin(one_role) {
 
     const arrow = $('#__next > div > div.main-content > div.sc-1a736df3-0.PimrZ.cmc-body-wrapper > div > div.sc-aef7b723-0.jfPVkR.container > div.sc-fe06e004-0.jYNuJy > div > div.sc-aef7b723-0.dDQUel.priceSection > div.sc-aef7b723-0.dDQUel.priceTitle > span').contents().get(0).attribs.class
 
-    roles = ['1070278187407388763', '1011635113928429651', '1070448519082680372']
-    another_roles = ['1070376219230601316', '1052981220469919774', '1070448519082680372']
-
     if(arrow == `icon-Caret-up`) {
-        roles.push(`1070308288404672612`)
+        dtc_roles.push(`1070308288404672612`)
         nick = `▲`
     } else {
-        roles.push(`1070308485058801724`)
+        dtc_roles.push(`1070308485058801724`)
         nick = `▼`
     }
 
