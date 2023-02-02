eab051080beb07d4e7967727e56809f6f44f904f
chainsmoker-codes <himeshkumardns@gmail.com>
2023-02-02 15:02:13 +0530

    Update dji.js

--- src/dji.js
+++ src/dji.js
@@ -5,7 +5,7 @@ async function dji() {
     const response = await fetch(`https://www.marketwatch.com/investing/index/djia`)
     const body = await response.text()
     const $ = cheerio.load(body)
-    const data = $('#maincontent > div.region.region--intraday > div.column.column--aside > div > div.intraday__data > h2 > span').contents().first().text()
+    const price = $('#maincontent > div.region.region--intraday > div.column.column--aside > div > div.intraday__data > h2 > span').contents().first().text()
     const percent = $('#maincontent > div.region.region--intraday > div.column.column--aside > div > div.intraday__data > bg-quote > span.change--percent--q').contents().first().text()
 
     roles = ['1070278187407388763', '1011635113928429651', '1070456854024564768']
